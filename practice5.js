c999103b86b5c398b8591baf0fc95af3fc5efa5e
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-18 12:19:04 +0530

    practice

--- practice5.js
+++ practice5.js
@@ -5,3 +5,38 @@
 // - High score localStorage
 // - Question shuffling
 // - Progress bar
+
+const questions = [
+  {
+    question: "What does 'this' refer to in object method?",
+    options: ["Window", "Object itself", "Parent function", "Undefined"],
+    correct: 1,
+    difficulty: "easy",
+  },
+];
+
+class Quiz {
+  constructor(question, options, correct, difficulty) {
+    this.id = 0;
+    this.question = question;
+    this.options = options;
+    this.correct = correct;
+    this.difficulty = difficulty;
+  }
+}
+
+class QuizApp {
+  constructor() {
+    this.init();
+    this.questions = []; // The 10 selected for this round
+    this.currentIndex = 0;
+    this.score = 0;
+    this.timer = null; // Variable to hold the setInterval
+    this.timeLeft = 60;
+  }
+
+  init() {
+    this.loadFromStorage();
+    this.render();
+  }
+}
