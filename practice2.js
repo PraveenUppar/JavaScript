a74fcdff01431260242261c6e61376427516edf5
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-18 11:45:23 +0530

    practice

---
+++ practice2.js
@@ -0,0 +1,8 @@
+// - User authentication simulation
+// - Post tweets (280 char limit)
+// - Like/Retweet/Reply system
+// - Follow/Unfollow users
+// - Real-time updates (setInterval simulation)
+// - Trending topics
+// - Infinite scroll feed
+// - Dark/Light theme toggle
--- practice4.js
+++ practice4.js
@@ -3,6 +3,84 @@
 // - Calculate total spent, spent by category
 // - Filter by date range
 // - Delete/edit expenses
-// - Show pie chart of spending by category (use Chart.js)
-// - Export to CSV
 // - Persist all data in localStorage
+
+[
+  {
+    id: 0,
+    title: "Buy milk",
+    amount: 100,
+    category: "Food",
+    date: 12 - 12 - 2035,
+  },
+];
+
+class Expense {
+  // Pass all data needed to create one receipt
+  constructor(title, amount, category, date) {
+    this.id = Date.now();
+    this.title = title;
+    this.amount = Number(amount);
+    this.category = category;
+    this.date = new Date(date);
+  }
+}
+class ExpenseTracker {
+  constructor() {
+    this.expenses = [];
+    this.init();
+  }
+
+  init() {
+    this.loadFromStorage();
+    this.render();
+  }
+
+  addExpense(title, amount, category, date) {
+    const newExpense = new Expense(title, amount, category, date);
+    this.expenses.push(newExpense);
+    this.saveToStorage();
+    this.render();
+  }
+
+  deleteExpense(id) {
+    this.expenses = this.expenses.filter((expense) => {
+      expense.id !== id;
+    });
+  }
+
+  editExpense(id, updatedTitle, updatedAmount, updatedCategory, updatedDate) {
+    this.expenses = this.expenses.map((expense) => {
+      if (expense.id === id) {
+        return {
+          id: id, // Keep the original ID!
+          title: updatedTitle,
+          amount: Number(updatedAmount),
+          category: updatedCategory,
+          date: new Date(updatedDate),
+        };
+      } else {
+        return expense;
+      }
+    });
+    this.saveToStorage();
+    this.render();
+  }
+
+  calculateTotal() {
+    let total = 0;
+    for (const expense of this.expenses) {
+      total += expense.amount;
+    }
+    return total;
+  }
+
+  filterByDate(startDate, endDate) {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    return this.expenses.filter((expense) => {
+      const expenseDate = new Date(expense.date);
+      return expenseDate >= start && expenseDate <= end;
+    });
+  }
+}
