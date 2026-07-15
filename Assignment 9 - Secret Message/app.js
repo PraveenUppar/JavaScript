d36ea31578533e9581fc361ea6a81af748ff1a08
Praveen Uppar <praveenuppar718@gmail.com>
2026-07-15 12:10:21 +0530

    assignments todos

---
+++ Assignment 9 - Secret Message/app.js
@@ -0,0 +1,9 @@
+Using array methods, you will transform an array of strings into a secret message!
+
+You should consult the Mozilla Developer Network (MDN) for reference on any method with which you are not familiar.
+
+If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck” to see a project walkthrough video.
+
+Code review available when you’re done
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
